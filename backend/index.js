5affbad0c7dc536a7fe04335e5ac02bdeaf976f0
charugundla_chakri_raman <charugundla.chakri.raman@oracle.com>
2025-03-17 10:30:24 +0530

    docker files updated for building in vm

--- backend/index.js
+++ backend/index.js
@@ -35,4 +35,4 @@ app.use("/questions", questionRoutes);
 app.use("/responses", responseRoutes);
 app.use("/dashboard", dashboardRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(5123, () => console.log("Server running on port 5123"));
